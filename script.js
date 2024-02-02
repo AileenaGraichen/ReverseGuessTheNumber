32760b016726d5c358df920a1040664e2949445d
Aileena <aile0074@stud.kea.dk>
2024-02-02 10:18:25 +0100

    update styling

--- script.js
+++ script.js
@@ -1,41 +1,5 @@
 "use strict";
 
-/*window.addEventListener("load", start);
-let number;
-
-function start() {
-  console.log("JavaScript is running");
-  number = generateRandomNumber();
-    console.log(number);
-    displayGuess();
-}
-
-function generateRandomNumber() {
-    return 42;
-}
-
-function displayGuess() {
-    console.log("displayGuess");
-    const guessElement = document.querySelector("#guess");
-    guessElement.textContent = number;
-}
-
-function checkGuess() {
-    console.log("checkGuess");
-}
-
-function guessIsCorrect() {
-    console.log("guessIsCorrect");
-}
-
-function guessIsTooHigh() {
-    console.log("guessIsTooHigh");
-}
-
-function guessIsTooLow() {
-    console.log("guessIsTooLow");
-}*/
-
 let lowerBound = 1;
 let upperBound = 100;
 let currentGuess = 50;
@@ -56,9 +20,7 @@ function lower() {
 }
 
 function correct() {
-    alert("Hooray! I guessed it: " + currentGuess);
-    // Reset the game or add additional functionality here if desired
+    alert("Yay! " + currentGuess);
 }
 
-// Initialize the first guess
 updateGuess();
