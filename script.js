0de0db3443e4b2ec97544e43624ddfb4d5a8b763
Aileena <aile0074@stud.kea.dk>
2024-02-08 14:42:49 +0100

    setup alerts + bounds

--- script.js
+++ script.js
@@ -7,14 +7,23 @@ let currentGuess = 50;
 function updateGuess() {
     currentGuess = Math.floor((lowerBound + upperBound) / 2);
     document.getElementById('guess').textContent = currentGuess;
+    
 }
 
 function higher() {
+    if(currentGuess === upperBound) {
+        alert("Upper bound reached");
+        return;
+    } 
     lowerBound = currentGuess + 1;
     updateGuess();
 }
 
 function lower() {
+    if(currentGuess === lowerBound) {
+        alert("Lower bound reached");
+        return;
+    } 
     upperBound = currentGuess - 1;
     updateGuess();
 }
